1d5978c3346e53c43202b9b69d1c5249fcf057b8
JoeyShapiro <joeyashapiro@gmail.com>
2025-04-10 20:37:18 -0500

    fixed stuff

--- dpath.mjs
+++ dpath.mjs
@@ -1,27 +1,45 @@
+import { readFileSync } from 'fs';
+import { argv } from 'process';
+
+const args = argv.slice(2);
 const data = readFileSync(args[0], 'utf8');
+
 let stack = [];
 var tag = '';
 var inStartTag = false;
 var inEndTag = false;
+var inTag = false;
 var line = 1;
+
 for (let i = 0; i < data.length; i++) {
     const c = data[i];
     if (line == args[1]) break;
+
     switch (c) {
         case '<':
             if (data[i + 1] == '?') break;
             if (data[i + 1] == '/') inEndTag = true;
-            else inStartTag = true;
+            else {
+                inStartTag = true;
+                inTag = true;
+            }
             break;
         case '>':
             if (data[i - 1] != '/') {
-                if (inStartTag) stack.push([ tag, line ]);
+                if (inStartTag) stack.push([tag, line]);
                 else if (inEndTag) stack.pop();
             }
             tag = '';
             inStartTag = false;
             inEndTag = false;
             break;
+        case '\n':
+            line++;
+            break;
+        default:
+            if (inStartTag && c == ' ') inTag = false;
+            else if (inStartTag && inTag) tag += c;
     }
 }
+
 console.log(stack);
