2667dc089e8d354e4cf6589ad68bcc86a860a11b
JoeyShapiro <joeyashapiro@gmail.com>
2025-04-12 16:59:22 -0500

    use fd instead of read

--- src/dpath.ts
+++ src/dpath.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { close, openSync, readSync } from 'fs';
 
 export function DeepPath(filename: string, filetype: string, line: number): Array<[string, number]> {
     if (filetype == 'xml') {
@@ -8,7 +8,8 @@ export function DeepPath(filename: string, filetype: string, line: number): Arra
     throw new Error(`Unsupported file type: ${filetype}`);
 }
 
-export function XmlTag(filename: string, line: number): Array<[string, number]> {
+export function XmlTag(filename: string, line: number, size: number = 1_048_576): Array<[string, number]> {
+    // TODO store list of whole thing
     let stack: Array<[string, number]> = [];
     var tag = '';
     var inStartTag = false;
@@ -16,55 +17,65 @@ export function XmlTag(filename: string, line: number): Array<[string, number]> 
     var inTagName = false;
     var curline = 1;
 
-    const data = readFileSync(filename, 'utf8');
+    var data = Buffer.alloc(size);
+    const f = openSync(filename, 'r');
+    var n = readSync(f, data, 0, data.length, null);
 
-    for (let i = 0; i < data.length; i++) {
-        const c = data[i];
-        if (curline == line+1) break;
+    while (n > 0) {
+        if (curline == line + 1) break;
 
-        switch (c) {
-            case '<':
-                if (data[i + 1] == '?') break;
-                if (data[i + 1] == '/') inEndTag = true;
-                else {
-                    inStartTag = true;
-                    inTagName = true;
-                }
-                break;
-            case '>':
-                if (data[i - 1] == '/' && curline != line) {
-                    stack.pop();
-                } else if (inStartTag && inTagName) {
-                    // TODO store list of whole thing
-                    stack.push([tag, curline]);
-                } else if (inEndTag && curline != line) {
-                    stack.pop();
-                }
+        for (let i = 0; i < n; i++) {
+            // -_- String(60) == '60' not '<'
+            const c = String.fromCharCode(data[i]);
+            if (curline == line + 1) break;
 
-                tag = '';
-                inTagName = false;
-                inStartTag = false;
-                inEndTag = false;
-                break;
-            case '\n':
-                if (inStartTag && tag) {
-                    stack.push([tag, curline]);
+            switch (c) {
+                case '<':
+                    if (String.fromCharCode(data[i + 1]) == '?') break;
+                    if (String.fromCharCode(data[i + 1]) == '/') inEndTag = true;
+                    else {
+                        inStartTag = true;
+                        inTagName = true;
+                    }
+                    break;
+                case '>':
+                    if (String.fromCharCode(data[i - 1]) == '/' && curline != line) {
+                        stack.pop();
+                    } else if (inStartTag && inTagName) {
+                        stack.push([tag, curline]);
+                    } else if (inEndTag && curline != line) {
+                        stack.pop();
+                    }
+
                     tag = '';
                     inTagName = false;
-                }
-                curline++;
-                break;
-            case ' ':
-                if (inStartTag && tag) {
-                    stack.push([tag, curline]);
-                    tag = '';
-                    inTagName = false;
-                }
-                break;
-            default:
-                if (inStartTag && inTagName) tag += c;
+                    inStartTag = false;
+                    inEndTag = false;
+                    break;
+                case '\n':
+                    if (inStartTag && tag) {
+                        stack.push([tag, curline]);
+                        tag = '';
+                        inTagName = false;
+                    }
+                    curline++;
+                    break;
+                case ' ':
+                    if (inStartTag && tag) {
+                        stack.push([tag, curline]);
+                        tag = '';
+                        inTagName = false;
+                    }
+                    break;
+                default:
+                    if (inStartTag && inTagName) tag += c;
+            }
         }
+
+        n = readSync(f, data, 0, data.length, null);
     }
+
+    close(f);
 
     return stack;
 }
